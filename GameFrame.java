a28ba26b9639a63de395afbe19aa36274834fad6
b13505020 <wangxiangmao1@gmail.com>
2026-05-04 13:46:38 +0800

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -1,17 +1,42 @@
+import java.awt.CardLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GameFrame extends JFrame {
 
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
+
+    private StartMenuPanel startMenuPanel;
+    private GamePanel gamePanel;
+
     public GameFrame() {
-        this.setTitle("Deep Sea Hunter");
+        this.setTitle("深海工域");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
 
-        GamePanel panel = new GamePanel();
-        this.add(panel);
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+        startMenuPanel = new StartMenuPanel(this);
+        gamePanel = new GamePanel();
+
+        mainPanel.add(startMenuPanel, "START_MENU");
+        mainPanel.add(gamePanel, "GAME");
+
+        this.add(mainPanel);
         this.pack();
 
         this.setLocationRelativeTo(null);
         this.setVisible(true);
+    }
+
+    public void startGame() {
+        cardLayout.show(mainPanel, "GAME");
+        gamePanel.requestFocusInWindow();
+    }
+
+    public void showStartMenu() {
+        cardLayout.show(mainPanel, "START_MENU");
     }
 }
